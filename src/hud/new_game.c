94aacd28f9847361cfd96c0f46be1f566d501b39
Clément <clement.baudonnel@epitech.eu>
2018-03-23 01:46:38 +0100

    Origin clemdev(features) starting new_game scene

--- src/hud/new_game.c
+++ src/hud/new_game.c
@@ -11,8 +11,8 @@ int new_game(my_w_t *window)
 {
 	int error_no = 0;
 
-	window->current = hm_get_bucket(window->scenes, "GAME");
-	error_no = clean_displayed_scenes_and_add_back(window, "GAME");
+	window->current = hm_get_bucket(window->scenes, "NEW_GAME");
+	error_no = clean_displayed_scenes_and_add_back(window, "NEW_GAME");
 	if (error_no == 84)
 		return (84);
 	return (1);
