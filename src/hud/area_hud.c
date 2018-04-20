de10ca2a6df2f9634b46c2eda28f47f184c68102
Lucas Sanchez <lucas.sanchez@epitech.eu>
2018-04-20 10:10:13 +0200

    Commit area hud

--- src/hud/area_hud.c
+++ src/hud/area_hud.c
@@ -10,10 +10,10 @@
 int change_area_hud(my_w_t *window)
 {
 	scene_t *area = hm_get(window->scenes, "AREA_HUD");
-	sfText *pos = hm_get(area->texts, "POS");
+	sfText *pos_area = hm_get(area->texts, "POS");
 
-	if (!pos)
+	if (!pos_area)
 		return (0);
-	sfText_setString(pos, my_strcat(int_to_str(AREA_COOR_X),
+	sfText_setString(pos_area, my_strcat(int_to_str(AREA_COOR_X),
 				my_strcat(",", int_to_str(AREA_COOR_Y))));
 }
