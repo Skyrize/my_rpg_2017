39efcda6f90eb0a61ed262427d24331a60fe61f5
simon1 <simon1.provost@epitech.eu>
2018-05-12 22:56:25 +0200

    add correction -h

--- src/error_handlings_args.c
+++ src/error_handlings_args.c
@@ -35,11 +35,11 @@ int check_help_programm(char **av)
 		return (84);
 	else if (av[1] && my_strcmp(av[1], "-h") == 0) {
 		write_help_instruction();
-		return (1);
+		return (0);
 	}
 	if (av[1] && my_strcmp(av[1], "-v") == 0) {
 		my_printf("################### V 1 . 0 ###################\n");
-		return (1);
+		return (0);
 	}
 	return (0);
 }
